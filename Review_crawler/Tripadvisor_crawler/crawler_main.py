50891217a5480adef99a34eed3388c74ac6d3f1a
donghyun-daniel <donghyun.main@gmail.com>
2021-02-20 15:31:14 +0900

    updated tripadvisor_crawler

--- Review_crawler/Tripadvisor_crawler/crawler_main.py
+++ Review_crawler/Tripadvisor_crawler/crawler_main.py
@@ -1,3 +1,5 @@
+# coding:utf-8
+
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver import ChromeOptions
@@ -8,45 +10,73 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-def get_hotel_review(review_list):
-    time.sleep(3)
-    page = 0
+def init_page_setting(driver):
+    filter_hotel_xpath = '//*[@id="component_10"]/div/div[2]/div[5]/div[2]/div[1]/div/label'
+    driver.find_element_by_xpath(filter_hotel_xpath).click()  # 호텔 필터 클릭
+    time.sleep(5)
 
-    comment_tags = [driver.find_elements_by_css_selector('h1._1mTlpMC3'),  # Hotel name
-                    driver.find_elements_by_css_selector('q.IRsGHoPm'),  # review plain txt
-                    driver.find_elements_by_css_selector('div._1EpRX7o3'),  # Location, Posting, Num of Reviews
-                    driver.find_elements_by_css_selector('div._2fxQ4TOx')]  # ID, Date
+    open_view_box_xpath = '/html/body/div[2]/div[1]/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/span[1]/div/div/div[1]'
+    driver.find_element_by_xpath(open_view_box_xpath).click()
+    time.sleep(1)
+
+    sort_distance_xpath = '/html/body/div[2]/div[1]/div[1]/div/div[2]/div[2]/div/div[2]/div[2]/span[1]/div/div[2]/div[4]/div'
+    driver.find_element_by_xpath(sort_distance_xpath).click()
+    time.sleep(5)
+
+def click_hotel_next_btn(driver):
+    next_btn_xpath = '//*[@class="unified ui_pagination standard_pagination ui_section listFooter"]/a'
+    next_btn = driver.find_elements_by_xpath(next_btn_xpath)  # next page
+    next_btn[-1].send_keys(Keys.ENTER)
+    time.sleep(5)
+
+def get_hotel_review(driver):
+    review_list = []
+
+    hotel_name = driver.find_element_by_xpath('//*[@id="HEADING"]').text
+    hotel_address = driver.find_element_by_xpath(
+        '//*[@id="component_4"]/div/div/div[2]/div/div[2]/div/div/div').text
+    hotel_rating = float(driver.find_element_by_css_selector('span._3cjYfwwQ').text)
 
     while True:
-        page += 1
-        # 더보기 전부 클릭
-        more_btns = driver.find_elements_by_css_selector('span._3maEfNCR')
+
         try:
-            for i in more_btns:
-                i.send_keys(Keys.ENTER)
+            # 더보기 버튼 누르기 시도
+            more_btn = driver.find_element_by_css_selector('div.XUVJZtom')
+            driver.execute_script("arguments[0].click();", more_btn)
+
         except:
-            pass
+            # 더보기 버튼이 존재하지 않으면 가져올 review가 없으므로 바로 탈출
+            driver.close()
+            return [[0]]
 
-        hotel_name = driver.find_element_by_css_selector('h1._1mTlpMC3').text
-        hotel_address = driver.find_element_by_xpath('//*[@id="component_4"]/div/div[1]/div[2]/div/div[2]/div/div[1]/div/span[2]/span').text
+        time.sleep(1)
 
-        comment_tags = [driver.find_elements_by_css_selector('span._3cjYfwwQ'), # Hotel Rating
-                        driver.find_elements_by_css_selector('div._2fxQ4TOx'), # Date
-                        driver.find_elements_by_xpath('//*[@id="component_14"]/div/div[3]/div[4]/div[3]/div[1]/div/span'), # Review Rating
-                        driver.find_elements_by_css_selector('div.glasR4aX'), # Review Title
+        # 추출할 속성들 정리
+        comment_tags = [driver.find_elements_by_css_selector('div._2fxQ4TOx'),  # Date
+                        driver.find_elements_by_css_selector('div.nf9vGX55'), # Review Rating
+                        driver.find_elements_by_xpath("//div[@class='_2UEC-y30']/div/span"),  # Review Title
                         driver.find_elements_by_css_selector('q.IRsGHoPm')  # review plain txt
                         ]
 
-        for hr, d, rr, rt, rpt in zip(*comment_tags):
-            review_list.append([hotel_name, hotel_address, float(hr.text)*2, preprocessing_date(d.text), float(rr.get_attribute('class')[-2:])/5, rt.text, rpt.text])
-        try:
-            next_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located([By.CSS_SELECTOR, 'a.nav.next']))  # next page
-            next_btn.send_keys(Keys.ENTER)
+        # 추출한 속성 review_list에 추가
+        for d, rr, rt, rpt in zip(*comment_tags):
+            review_list.append([hotel_name, hotel_address, hotel_rating * 2, preprocessing_date(d.text),
+                                float(rr.get_attribute('class')[-2:]) / 5, rt.text, rpt.text])
+            print([hotel_name, hotel_address, hotel_rating * 2, preprocessing_date(d.text),
+                                float(rr.get_attribute('class')[-2:]) / 5, rt.text, rpt.text])
+        # 다음을 누를 수 있으면 다음
+        try :
+            review_next_btn = driver.find_element_by_css_selector('a.nav.next')
+            review_next_btn.click()
+            time.sleep(4)
+
+        # 다음을 누를 수 없으므로 탈출
         except:
             break
-        if page == 2:  # 테스트, 2페이지만
-            break
-        time.sleep(1)
+
+    driver.close()
+
+    return review_list
 
 def preprocessing_date(date):
     try :
@@ -56,65 +86,63 @@ def preprocessing_date(date):
     except:
         return str(now_date.tm_year) + "-" + str(now_date.tm_mon)
 
-
-now_date = time.localtime(time.time())
-
+# url 접속
 url = 'https://www.tripadvisor.co.kr/Hotels-g294197-Seoul-Hotels.html'
 options = ChromeOptions()
-options.add_argument('headless')
-
+#options.add_argument('headless')
 driver = Chrome("chromedriver", options=options)
 driver.set_window_size(2560,1440)
 driver.get(url)
+time.sleep(5)
 
+# filter, sort 적용
+init_page_setting(driver)
+
+page_size = int(driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div/div[2]/div[3]/div[2]/div[9]/div/div/div/div/a[7]').text)
+
+time.sleep(5)
+
+# 리뷰 중 날짜가 최근인 데이터들을 맞춰주기 위해
+now_date = time.localtime(time.time())
+now_year, now_mon = now_date.tm_year, now_date.tm_mon
+
+# csv 저장 편리하게 하기 위해
 page_idx = 1
 
-while True:
-    review_list = []
-    hotel_name_list = []
-    hotel_name_xpath = '//*[@id="property_7686969"]'
-    hotel_name = driver.find_elements_by_xpath('//*[@id="property_7686969"]')
-    page = driver.find_elements_by_css_selector('a.review_count')
-    idx = 0
+# hotel 리스트 다음 버튼을 page_size - 1번 누른다
+for page_num in range(page_size - 1):
 
-    for i in hotel_name:
-        hotel_name_list.append(i.text)
+    # 호텔 리스트 페이지에서 리뷰가 있다면 각 호텔 리뷰 페이지로 접속
+    hotel_pages = driver.find_elements_by_css_selector('a.review_count')
+    hotels_review_list = []
+    print(len(hotel_pages))
+    for hp_idx in range(len(hotel_pages)):
+        # review가 0개보다 클 경우 클릭
+        time.sleep(3)
+        hotel_pages = driver.find_elements_by_css_selector('a.review_count')
+        if hotel_pages[hp_idx].get_attribute('class') == "review_count":
+            hotel_pages[hp_idx].send_keys(Keys.ENTER)
+            time.sleep(5)
+            driver.switch_to.window(driver.window_handles[-1])
 
-    # 현재 page에서 보이는 모든 호텔들 다 클릭 후 새창 띄우기
-    for i in range(len(page)):
-        time.sleep(5)
-        try:
-            page[idx].send_keys(Keys.ENTER)
-        except:
-            page = driver.find_elements_by_css_selector('a.review_count')
-            page[idx].send_keys(Keys.ENTER)
+            # review가 있는 hotel page에서 review 가져오기 시작
+            tmp = get_hotel_review(driver)
+            print(len(tmp[0]))
+            if len(tmp[0]) > 1 :
+                hotels_review_list += tmp
+            driver.switch_to.window(driver.window_handles[0])
 
-        time.sleep(5)
+    for i in hotels_review_list:
+        print(i)
 
-        driver.switch_to.window(driver.window_handles[-1])
-        get_hotel_review(review_list)
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        idx += 1
+    click_hotel_next_btn(driver)
 
-    try:
-        time.sleep(5)
-        next_btn_xpath = '//*[@class="unified ui_pagination standard_pagination ui_section listFooter"]/a'
-        next_btn = driver.find_elements_by_xpath(next_btn_xpath)  # next page
-        next_btn[-1].send_keys(Keys.ENTER)
-        time.sleep(5)
-
-    except:
-        break
-
-    page_idx += 1
-    print(hotel_name_list)
-
-    # Error control이 100% 되기는 힘드니, page별로 나눠 csv 생성성
-    df = pd.DataFrame(review_list)
+    df = pd.DataFrame(hotels_review_list)
     df.to_csv(f'../csv_output/Tripadvisor{page_idx}.csv',
               index=False,
               header=["HotelName", "HotelAddress", "HotelRating", "ReviewDate", "ReviewRating", "ReviewTitle",
                       "ReviewText"])
+
+    page_idx += 1
 
 driver.close()
